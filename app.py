13e77903a667d6a9aa4900b4d2a56c37ff4502ee
Vinip1203 <viniciuspozzan@hotmail.com>
2025-11-03 22:32:31 -0300

    corrige host

--- app.py
+++ app.py
@@ -63,4 +63,5 @@ def index():
     return redirect("/login")
 
 if __name__ == "__main__":
-    app.run(debug=False)  # ← DEBUG DESLIGADO para produção
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)  # ← DEBUG DESLIGADO para produção
